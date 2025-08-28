5bf0c87b4797664f458d9282b0ad1410c6ef6439
Izumi910 <sebastian18abalos@gmail.com>
2025-08-28 12:14:51 -0400

    actualizacion nose

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class dispositivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo = models.IntegerField(default=0)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+    
+
